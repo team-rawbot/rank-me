1fb3b25b2600b851a901a8f41017357d452b9ad3
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2013-07-23 22:50:16 +0200

    use signals to update ranks, use managers for objects-related operations

--- game/admin.py
+++ game/admin.py
@@ -2,15 +2,5 @@ from django.contrib import admin
 from game.models import Game, Rank
 
 
-class GameAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        Rank.update_rank(obj.winner.id, obj.loser.id)
-        obj.save()
-
-admin.site.register(Game, GameAdmin)
-
-
-class RankAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Rank, RankAdmin)
+admin.site.register(Game)
+admin.site.register(Rank)
--- game/views.py
+++ game/views.py
@@ -1,16 +1,17 @@
 from django.http import Http404
 from django.shortcuts import render
-from trueskill import Rating, rate_1vs1
 from game.models import Game, Rank
 
+
 def index(request):
-    latest_results = Game.get_latest_results()
-    score_board = Rank.get_score_board()
+    latest_results = Game.objects.get_latest()
+    score_board = Rank.objects.get_score_board()
     context = {
         'latest_results': latest_results,
         'score_board': score_board
     }
     return render(request, 'game/index.html', context)
+
 
 def detail(request, game_id):
     try:
