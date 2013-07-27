f1957a176858776c46ddf6ee881850432196e511
ghn <ghugon@gmail.com>
2013-07-27 12:46:18 +0200

    use grunt to compute LESS files

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["static/assets/less"],
+                    compress : false,
+                    yuicompress : false
+                },
+                files: {
+                    "static/main.css": "static/main.less"
+                }
+            }
+        },
+
+        watch: {
+            assets: {
+                files: "static/**/*.less",
+                tasks: ["less"],
+                options: {
+                    debounceDelay: 250,
+                }
+            }
+        },
+    });
+
+    // Default task(s).
+    grunt.registerTask("default", ["less", "watch"]);
+};
