a00c89a08bfa30525d126d64add29c6fa84ae285
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2013-12-19 09:44:35 +0100

    use system timezone instead of Chicago

--- rankme/settings/base.py
+++ rankme/settings/base.py
@@ -26,7 +26,7 @@ BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
