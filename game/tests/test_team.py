a7b49d341bcef490b1a48031f8875733ecaf1fbb
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2013-12-22 21:34:30 +0100

    don't use assertItemsEqual since it's not available in Python 3

--- game/tests/test_team.py
+++ game/tests/test_team.py
@@ -10,27 +10,36 @@ class TestTeamGetOrCreate(TestCase):
         # Create 4 dummy users
         self.users = [UserFactory() for id in range(4)]
 
+    def assertUsersEqual(self, first, second):
+        def users_to_id_list(users):
+            return sorted([user.id for user in users])
+
+        first_ids = users_to_id_list(first)
+        second_ids = users_to_id_list(second)
+
+        return len(first_ids) == len(second_ids) and first_ids == second_ids
+
     def test_team_creation(self):
         team, created = Team.objects.get_or_create_from_players(
             (self.users[0].id, self.users[1].id)
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 1)
-        self.assertItemsEqual(team.users.all(), self.users[0:2])
+        self.assertUsersEqual(team.users.all(), self.users[0:2])
 
         team, created = Team.objects.get_or_create_from_players(
             self.users[0].id
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 2)
-        self.assertItemsEqual(team.users.all(), [self.users[0]])
+        self.assertUsersEqual(team.users.all(), [self.users[0]])
 
         team, created = Team.objects.get_or_create_from_players(
             self.users[1].id
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 3)
-        self.assertItemsEqual(team.users.all(), [self.users[1]])
+        self.assertUsersEqual(team.users.all(), [self.users[1]])
 
     def test_team_uniqueness(self):
         """
@@ -42,7 +51,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 1)
-        self.assertItemsEqual(team.users.all(), self.users[0:2])
+        self.assertUsersEqual(team.users.all(), self.users[0:2])
 
         # Check that team (0, 1) is equal to team (0, 1)
         team, created = Team.objects.get_or_create_from_players(
@@ -50,7 +59,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertFalse(created)
         self.assertEqual(Team.objects.count(), 1)
-        self.assertItemsEqual(team.users.all(), self.users[0:2])
+        self.assertUsersEqual(team.users.all(), self.users[0:2])
 
         # Check that team (1, 0) is equal to team (0, 1)
         team, created = Team.objects.get_or_create_from_players(
@@ -58,7 +67,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertFalse(created)
         self.assertEqual(Team.objects.count(), 1)
-        self.assertItemsEqual(team.users.all(), self.users[0:2])
+        self.assertUsersEqual(team.users.all(), self.users[0:2])
 
         # Check that team (1, 2) is different from team (0, 1)
         team, created = Team.objects.get_or_create_from_players(
@@ -66,7 +75,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 2)
-        self.assertItemsEqual(team.users.all(), self.users[1:3])
+        self.assertUsersEqual(team.users.all(), self.users[1:3])
 
     def test_team_overlap(self):
         """
@@ -78,7 +87,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 1)
-        self.assertItemsEqual(team.users.all(), self.users[0:2])
+        self.assertUsersEqual(team.users.all(), self.users[0:2])
 
         # Check that team (0, 1, 2) is different from (0, 1)
         team, created = Team.objects.get_or_create_from_players(
@@ -86,7 +95,7 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 2)
-        self.assertItemsEqual(team.users.all(), self.users[0:3])
+        self.assertUsersEqual(team.users.all(), self.users[0:3])
 
         # Check that team (1, 2) is different from (0, 1, 2)
         team, created = Team.objects.get_or_create_from_players(
@@ -94,4 +103,4 @@ class TestTeamGetOrCreate(TestCase):
         )
         self.assertTrue(created)
         self.assertEqual(Team.objects.count(), 3)
-        self.assertItemsEqual(team.users.all(), self.users[1:3])
+        self.assertUsersEqual(team.users.all(), self.users[1:3])
