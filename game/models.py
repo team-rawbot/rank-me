dd185b442528ce48cdba313edc031285e207d342
Laurent Prodon <laurent@sitronnier.com>
2014-03-03 23:17:04 +0100

    move logic to model and refactor to get for each game a result object (vs only the skill)

--- game/models.py
+++ game/models.py
@@ -1,8 +1,11 @@
+import operator
+from trueskill import Rating, rate_1vs1
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
-from trueskill import Rating, rate_1vs1
+from django.db.models import Q
 
 
 class TeamManager(models.Manager):
@@ -53,8 +56,11 @@ class Team(models.Model):
 
     objects = TeamManager()
 
-    def __unicode__(self):
+    def get_name(self):
         return u" / ".join([user.username for user in self.users.all()])
+
+    def __unicode__(self):
+        return self.get_name()
 
 
 class GameManager(models.Manager):
@@ -126,7 +132,82 @@ class Game(models.Model):
         )
 
 
+class HistoricalScoreManager(models.Manager):
+    def get_latest(self, nb_games=50):
+        return (self.get_queryset()
+                .select_related('game', 'game__winner', 'game__loser')
+                .order_by('-id')[:nb_games])
+
+    def get_latest_results_by_team(self, nb_games=50):
+        """
+        Get nb_games latest scores for each team
+
+        :param nb_games:int number of games
+        :return:dict Dict with key=team and value=list of score objects
+
+        {team_a: [{skill: xx, played: xx, game: game_id}, ...]}
+        """
+        scores_by_team = {}
+
+        teams = Team.objects.all().select_related('winner', 'loser')
+
+        scores = self.get_latest(nb_games)
+        scores = sorted(scores, key=lambda score: score.id)
+        for score in scores:
+            all_skills_by_game = {}
+            for team in teams:
+                team_scores = scores_by_team.get(team, [])
+
+                result = {'game': score.game.id}
+
+                if team.id == score.game.winner_id:
+                    result['skill'] = score.winner_score
+                    result['win'] = True
+                    result['played'] = True
+                elif team.id == score.game.loser_id:
+                    result['skill'] = score.loser_score
+                    result['win'] = False
+                    result['played'] = True
+                else:
+                    result['played'] = False
+                    if len(team_scores) == 0:
+                        result['skill'] = self.get_last_score_for_team(team, scores[0].game)
+                    else:
+                        result['skill'] = team_scores[-1]['skill']
+
+                all_skills_by_game[team] = result['skill']
+
+                team_scores.append(result)
+                scores_by_team[team] = team_scores
+
+            positions_for_game = sorted(all_skills_by_game.iteritems(), key=operator.itemgetter(1), reverse=True)
+            for idx, position in enumerate(positions_for_game, start=1):
+                scores_by_team[position[0]][-1]['position'] = idx
+
+        return scores_by_team
+
+    def get_last_score_for_team(self, team, game):
+        """
+        Returns the latest skill before game :game for team :team
+
+        :param team:Team
+        :param game:Game
+        :return:skill
+        """
+        historical_score_id = game.historical_score.id
+        last_score = (self.get_queryset()
+                      .filter(Q(game__winner=team) | Q(game__loser=team))
+                      .filter(id__lte=historical_score_id)
+                      .select_related('game__winner', 'game__loser', 'game')
+                      .order_by('-id')
+                      .first())
+
+        return last_score.winner_score if last_score.game.winner == team else last_score.loser_score
+
+
 class HistoricalScore(models.Model):
-    game = models.OneToOneField(Game)
+    game = models.OneToOneField(Game, related_name='historical_score')
     winner_score = models.FloatField('Winner current score')
     loser_score = models.FloatField('Loser current score')
+
+    objects = HistoricalScoreManager()
