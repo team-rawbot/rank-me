f81c11489f9ada03ca8b33d8c4e1e3d5f60e25bc
Laurent Prodon <laurent@sitronnier.com>
2014-03-12 15:14:19 +0100

    add some js to ease result insertion

---
+++ static/assets/js/add_result.js
@@ -0,0 +1,35 @@
+$(function () {
+    // focus "add result" button on load
+    if ($('body').hasClass('home')) {
+        $('#add-result-button').focus();
+    }
+
+    if ($('body').hasClass('add-result')) {
+        $('body').keydown(function(event) {
+            var username = $('#nav-user-name').text();
+
+            // press 'w' to set yourself as winner
+            if (event.keyCode == 87) {
+                if (!selectOptionFromText('#id_winner', username)) {
+                    $('#id_loser').focus();
+                }
+            }
+            // press 'l' to set yourself as loser
+            else if (event.keyCode == 76) {
+                if (!selectOptionFromText('#id_loser', username)) {
+                    $('#id_winner').focus();
+                }
+            }
+        });
+    }
+
+    var selectOptionFromText = function(select, text) {
+        $(select + ' option:contains(' + text + ')').each(function(){
+            if ($(this).text() == text) {
+                $(this).attr('selected', 'selected');
+                return false;
+            }
+            return true;
+        });
+    };
+});
