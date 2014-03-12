ca5b3d5fd1849964d2c128a9f617c4524f3917c2
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-03-12 10:38:04 +0100

    head2head: try to fix Travis tests by not hardcoding ids

--- game/tests/test_results_team_page.py
+++ game/tests/test_results_team_page.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from game.models import Game
@@ -10,7 +11,9 @@ User = get_user_model()
 
 class TestResultsTeamPage(TestCase):
     def test_page_unavailability(self):
-        response = self.client.get('/results/team/1/')
+        response = self.client.get(
+            reverse('team_detail', kwargs={'team_id': 1})
+        )
         self.assertEqual(404, response.status_code)
 
     def test_page_availability(self):
@@ -19,9 +22,11 @@ class TestResultsTeamPage(TestCase):
         christoph = UserFactory()
 
         # create 1 game
-        Game.objects.announce(winner=sylvain, loser=christoph)
+        game = Game.objects.announce(winner=sylvain, loser=christoph)
 
-        response = self.client.get('/results/team/1/')
+        response = self.client.get(
+            reverse('team_detail', kwargs={'team_id': game.winner_id})
+        )
         self.assertEqual(200, response.status_code)
 
     def test_page_results(self):
@@ -32,15 +37,22 @@ class TestResultsTeamPage(TestCase):
         # create 3 game
         Game.objects.announce(winner=sylvain, loser=christoph)
         Game.objects.announce(winner=sylvain, loser=christoph)
-        Game.objects.announce(winner=christoph, loser=sylvain)
+        game = Game.objects.announce(winner=christoph, loser=sylvain)
 
-        response = self.client.get('/results/team/1/')
+        christoph_team_id = game.winner_id
+        sylvain_team_id = game.loser_id
+
+        response = self.client.get(
+            reverse('team_detail', kwargs={'team_id': sylvain_team_id})
+        )
 
         self.assertContains(response, '<ul class="team-statistics"')
         self.assertContains(response, '<ul class="head2head">')
         self.assertContains(response, '<li><strong>Longest Winning Streak</strong>: 2</li>')
 
-        response = self.client.get('/results/team/2/')
+        response = self.client.get(
+            reverse('team_detail', kwargs={'team_id': christoph_team_id})
+        )
 
         self.assertContains(response, '<ul class="team-statistics"')
         self.assertContains(response, '<ul class="head2head">')
