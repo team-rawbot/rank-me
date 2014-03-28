8cddd79c58098b743aad7423145f69b6262ea07f
ghn <ghugon@gmail.com>
2014-03-28 22:37:49 +0100

    Restrict access to logged users

--- game/views.py
+++ game/views.py
@@ -4,19 +4,26 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .forms import GameForm
 from .models import Game, Team, HistoricalScore
 
-
+"""
+User logged in => homepage
+User not logged => login page
+"""
 def index(request):
-    latest_results = Game.objects.get_latest()
-    score_board = Team.objects.get_score_board()
-    score_chart_data = HistoricalScore.objects.get_latest_results_by_team(50, True)
+    if request.user.is_authenticated():
+        latest_results = Game.objects.get_latest()
+        score_board = Team.objects.get_score_board()
+        score_chart_data = HistoricalScore.objects.get_latest_results_by_team(50, True)
 
-    context = {
-        'latest_results': latest_results,
-        'score_board': score_board,
-        'score_chart_data': score_chart_data,
-    }
+        context = {
+            'latest_results': latest_results,
+            'score_board': score_board,
+            'score_chart_data': score_chart_data,
+        }
 
-    return render(request, 'game/index.html', context)
+        return render(request, 'game/index.html', context)
+    else:
+        context = {}
+        return render(request, 'login/index.html', context)
 
 
 def detail(request, game_id):
