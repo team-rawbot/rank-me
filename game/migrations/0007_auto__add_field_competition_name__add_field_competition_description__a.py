4da7d50d8ca2b1ba3e2fab8ccea3b6f223dfafc0
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-05-16 11:56:07 +0200

    fix migrations

--- game/migrations/0007_auto__add_field_competition_name__add_field_competition_description__a.py
+++ game/migrations/0007_auto__add_field_competition_name__add_field_competition_description__a.py
@@ -6,12 +6,11 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
-    no_dry_run = True
 
     def forwards(self, orm):
         # Adding field 'Competition.name'
         db.add_column(u'game_competition', 'name',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=255),
+                      self.gf('django.db.models.fields.CharField')(default='Default competition', max_length=255),
                       keep_default=False)
 
         # Adding field 'Competition.description'
@@ -28,10 +27,6 @@ class Migration(SchemaMigration):
         db.add_column(u'game_competition', 'end_date',
                       self.gf('django.db.models.fields.DateTimeField')(null=True),
                       keep_default=False)
-
-        orm['game.Competition'].objects.filter(pk=1).update(
-            name='Default competition'
-        )
 
 
     def backwards(self, orm):
@@ -62,57 +57,8 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'game.competition': {
-            'Meta': {'object_name': 'Competition'},
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
-            'games': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['game.Game']", 'symmetrical': 'False'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'start_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'teams': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['game.Team']", 'through': u"orm['game.CompetitionTeam']", 'symmetrical': 'False'})
-        },
-        u'game.competitionteam': {
-            'Meta': {'object_name': 'CompetitionTeam'},
-            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['game.Competition']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'score': ('django.db.models.fields.FloatField', [], {'default': '25'}),
-            'stdev': ('django.db.models.fields.FloatField', [], {'default': '8.333'}),
-            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['game.Team']"})
-        },
-        u'game.game': {
-            'Meta': {'object_name': 'Game'},
-            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'loser': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'games_lost'", 'to': u"orm['game.Team']"}),
-            'winner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'games_won'", 'to': u"orm['game.Team']"})
-        },
-        u'game.historicalscore': {
-            'Meta': {'object_name': 'HistoricalScore'},
-            'game': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'historical_score'", 'unique': 'True', 'to': u"orm['game.Game']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'loser_score': ('django.db.models.fields.FloatField', [], {}),
-            'winner_score': ('django.db.models.fields.FloatField', [], {})
-        },
-        u'game.team': {
-            'Meta': {'object_name': 'Team'},
-            'defeats': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'score': ('django.db.models.fields.FloatField', [], {'default': '25'}),
-            'stdev': ('django.db.models.fields.FloatField', [], {'default': '8.333'}),
-            'users': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'teams'", 'symmetrical': 'False', 'to': u"orm['user.User']"}),
-            'wins': ('django.db.models.fields.IntegerField', [], {'default': '0'})
-        },
-        u'user.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
-            'avatar': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
@@ -126,6 +72,51 @@ class Migration(SchemaMigration):
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'game.competition': {
+            'Meta': {'object_name': 'Competition'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'games': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'competitions'", 'symmetrical': 'False', 'to': u"orm['game.Game']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'teams': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['game.Team']", 'through': u"orm['game.Score']", 'symmetrical': 'False'})
+        },
+        u'game.game': {
+            'Meta': {'object_name': 'Game'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'loser': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'games_lost'", 'to': u"orm['game.Team']"}),
+            'winner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'games_won'", 'to': u"orm['game.Team']"})
+        },
+        u'game.historicalscore': {
+            'Meta': {'object_name': 'HistoricalScore'},
+            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['game.Competition']"}),
+            'game': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'historical_scores'", 'to': u"orm['game.Game']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'score': ('django.db.models.fields.FloatField', [], {}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'historical_scores'", 'to': u"orm['game.Team']"})
+        },
+        u'game.score': {
+            'Meta': {'object_name': 'Score'},
+            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['game.Competition']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'score': ('django.db.models.fields.FloatField', [], {'default': '25'}),
+            'stdev': ('django.db.models.fields.FloatField', [], {'default': '8.333'}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'scores'", 'to': u"orm['game.Team']"})
+        },
+        u'game.team': {
+            'Meta': {'object_name': 'Team'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'teams'", 'symmetrical': 'False', 'to': u"orm['auth.User']"})
         }
     }
 
