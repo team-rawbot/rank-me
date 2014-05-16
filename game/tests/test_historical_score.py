30616b49f78a4725656d99c8dd6b8cadf33c74a4
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-05-16 12:47:59 +0200

    Use assertEqual instead of assertEquals in tests since it's deprecated in Python 3

--- game/tests/test_historical_score.py
+++ game/tests/test_historical_score.py
@@ -10,33 +10,33 @@ class TestHistoricalScore(TestCase):
         default_competition = Competition.objects.get_default_competition()
 
         game = Game.objects.announce(users[0], users[1], default_competition)
-        self.assertEquals(HistoricalScore.objects.count(), 2)
+        self.assertEqual(HistoricalScore.objects.count(), 2)
 
         historical_scores = HistoricalScore.objects.get_latest_results_by_team(
             10, default_competition
         )
-        self.assertEquals(len(historical_scores), 2)
-        self.assertEquals(historical_scores[game.winner][0]['position'], 1)
-        self.assertEquals(historical_scores[game.loser][0]['position'], 2)
+        self.assertEqual(len(historical_scores), 2)
+        self.assertEqual(historical_scores[game.winner][0]['position'], 1)
+        self.assertEqual(historical_scores[game.loser][0]['position'], 2)
         self.assertNotIn(users[2], historical_scores)
 
         Game.objects.announce(users[1], users[0], default_competition)
         game = Game.objects.announce(users[1], users[0], default_competition)
-        self.assertEquals(HistoricalScore.objects.count(), 6)
+        self.assertEqual(HistoricalScore.objects.count(), 6)
 
         historical_scores = HistoricalScore.objects.get_latest_results_by_team(
             10, default_competition
         )
-        self.assertEquals(len(historical_scores), 2)
+        self.assertEqual(len(historical_scores), 2)
 
-        self.assertEquals(historical_scores[game.winner][0]['position'], 2)
-        self.assertEquals(historical_scores[game.loser][0]['position'], 1)
-        self.assertEquals(historical_scores[game.winner][1]['position'], 1)
-        self.assertEquals(historical_scores[game.loser][1]['position'], 2)
-        self.assertEquals(historical_scores[game.winner][2]['position'], 1)
-        self.assertEquals(historical_scores[game.loser][2]['position'], 2)
+        self.assertEqual(historical_scores[game.winner][0]['position'], 2)
+        self.assertEqual(historical_scores[game.loser][0]['position'], 1)
+        self.assertEqual(historical_scores[game.winner][1]['position'], 1)
+        self.assertEqual(historical_scores[game.loser][1]['position'], 2)
+        self.assertEqual(historical_scores[game.winner][2]['position'], 1)
+        self.assertEqual(historical_scores[game.loser][2]['position'], 2)
 
         historical_scores = HistoricalScore.objects.get_latest_results_by_team(
             2, default_competition
         )
-        self.assertEquals(len(historical_scores[game.winner]), 2)
+        self.assertEqual(len(historical_scores[game.winner]), 2)
