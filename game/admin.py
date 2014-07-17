d97644832de939b503a76ec61715572964e59e6f
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2014-07-17 09:49:26 +0200

    Add Score and Competition models to the admin

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from game.models import Game, Team
+from game.models import Competition, Game, Score, Team
 
 
 admin.site.register(Game)
 admin.site.register(Team)
+admin.site.register(Score)
+admin.site.register(Competition)
