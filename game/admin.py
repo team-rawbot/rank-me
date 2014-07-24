594f61ab3cca560222ec0efac0c06cacd2f744d0
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2014-07-24 07:30:14 +0200

    Only display meaningful fields in competition admin

--- game/admin.py
+++ game/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from game.models import Competition, Game, Score, Team
 
 
+class CompetitionAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'start_date', 'end_date', 'slug']
+
 admin.site.register(Game)
 admin.site.register(Team)
 admin.site.register(Score)
-admin.site.register(Competition)
+admin.site.register(Competition, CompetitionAdmin)
