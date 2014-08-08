c48e9171529348eafd62cf1ce6cd74c9d7fb9b27
Noé Froidevaux <noe.froidevaux@gmail.com>
2014-08-08 17:02:41 +0200

    Add url to get an api token

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
-from .views import CompetitionViewSet
+from .views import competition_view_set
 
 router = routers.DefaultRouter()
-router.register(r'competitions', CompetitionViewSet)
+router.register(r'competitions', competition_view_set)
 
 urlpatterns = patterns(
     'api.views',
     url(r'^api/', include([
+        url(
+            r'^register-by-token/(?P<backend>[^/]+)/$',
+            'api.views.register_by_access_token',
+            name='register_by_access_token',
+        ),
         url(r'^', include(router.urls)),
         url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
         ])),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+import json
+from django.contrib.auth import login
+from django.http import HttpResponse
+from social.apps.django_app.utils import strategy
+from rest_framework.authtoken.models import Token
 from rest_framework import viewsets
 
 from game.models import Competition
@@ -5,7 +10,26 @@ from game.models import Competition
 from .serializers import CompetitionSerializer
 
 
-class CompetitionViewSet(viewsets.ModelViewSet):
+@strategy('social:complete')
+def register_by_access_token(request, backend):
+    backend = request.strategy.backend
+    token = {
+        'oauth_token': request.GET.get('oauth_token'),
+        'oauth_token_secret': request.GET.get('oauth_token_secret'),
+    }
+
+    user = backend.do_auth(token)
+    token, created = Token.objects.get_or_create(user=user)
+
+    data = {
+        'api_token': token.key
+    }
+
+    data_json = json.dumps(data)
+    return HttpResponse(data_json, mimetype='application/json')
+
+
+class competition_view_set(viewsets.ModelViewSet):
     """
     API endpoint that allows competitions to be viewed or edited.
     """
