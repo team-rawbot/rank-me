964a665c0f3b2456fbf2f83effbac7e4a7039b83
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-13 14:53:11 +0000

    Restore team graphs by moving it to require

--- static/assets/js/init.js
+++ static/assets/js/init.js
@@ -2,10 +2,12 @@ require([
     "jquery",
     "bootstrap",
     "modules/add_result",
+    "modules/team_charts",
     "modules/score_chart"
-], function($, Bootstrap, AddResult, ScoreChart) {
+], function($, Bootstrap, AddResult, TeamChart, ScoreChart) {
     $(function() {
         AddResult.init();
+        TeamChart.init();
         ScoreChart.drawChart($('#score-chart-container'));
     });
 });
