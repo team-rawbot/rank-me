110ac988d825f7a953fd8553bc2fe800eb7fc80b
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-09-03 07:28:15 +0000

    [REST] Teams test, use authentication

--- api/tests.py
+++ api/tests.py
@@ -2,13 +2,30 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from game.tests.factories import UserFactory
 
-class CompetitionTests(APITestCase):
+
+def test_route(self, route):
+    user = UserFactory()
+    self.client.force_authenticate(user)
+
+    url = reverse('competition-list')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertIsNotNone(response.data)
+
+
+class CompetitionListTests(APITestCase):
     def test_list_competitions(self):
         """
         Ensure we can list all competitions.
         """
-        url = reverse('competition-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIsNotNone(response.data)
+        test_route(self, 'competition-list')
+
+
+class PlayersListTests(APITestCase):
+    def test_list_players(self):
+        """
+        Ensure we can list all players.
+        """
+        test_route(self, 'team-list')
