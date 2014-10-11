ef0691e82a17b357555c65494537b5280f4a75e8
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-10-11 23:30:35 +0200

    Make sure all urls have a trailing slash

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'user.views',
     url(r'^$', 'index', name='profile'),
-    url(r'^edit$', 'edit', name='edit_profile'),
+    url(r'^edit/$', 'edit', name='edit_profile'),
 )
