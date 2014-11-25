3252848c72669b09bb5be7fb7f69f5167a70bb61
ghn <ghugon@gmail.com>
2014-11-25 14:51:40 +0100

    Improve display of game in timeline

--- timeline/models.py
+++ timeline/models.py
@@ -5,10 +5,22 @@ from django.db import models
 from django_hstore import hstore
 from game.models import Competition
 
+import json
+
 @receiver(game_played)
 def publish_game_played(sender, **kwargs):
+    winner = {
+        "id": sender.winner.id,
+        "name": sender.winner.get_name(),
+        "avatar": sender.winner.users.first().get_profile().avatar,
+    }
+    loser = {
+        "id": sender.loser.id,
+        "name": sender.loser.get_name(),
+        "avatar": sender.loser.users.first().get_profile().avatar
+    }
     event = Event(event_type="game", competition=sender.competitions.first(),
-        details={"winner": sender.winner.get_name(), "loser": sender.loser.get_name()}
+        details={"winner": winner, "loser": loser}
     )
     event.save()
 
@@ -18,6 +30,14 @@ class Event(models.Model):
     details = hstore.DictionaryField()
     date = models.DateTimeField(auto_now_add=True)
     competition = models.ForeignKey(Competition)
+
+    objects = hstore.HStoreManager()
+
+    def get_winner(self):
+        return json.loads(self.details["winner"])
+
+    def get_loser(self):
+        return json.loads(self.details["loser"])
 
     class Meta:
         ordering = ["-date"]
