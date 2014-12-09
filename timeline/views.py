24a38fc9b9715882052abd5819d9d042f8a655e8
ghn <ghugon@gmail.com>
2014-12-09 22:42:03 +0100

    Display user clubs on homepage

--- timeline/views.py
+++ timeline/views.py
@@ -10,6 +10,7 @@ def index(request):
     # Private homepage
     context = {
         'events': Event.objects.get_all_for_user(request.user),
-        'competitions': Competition.objects.get_visible_for_user(request.user)
+        'competitions': Competition.objects.get_visible_for_user(request.user),
+        'clubs': request.user.clubs.all()
     }
     return render(request, 'timeline/index.html', context)
