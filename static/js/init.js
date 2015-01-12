24fd6c21e131e08eb53eba474a01970eca4a144d
ghn <ghugon@gmail.com>
2015-01-12 19:44:57 +0100

    Show/hide archived competitions

--- static/js/init.js
+++ static/js/init.js
@@ -9,5 +9,18 @@ require([
         AddResult.init();
         TeamChart.drawChart();
         ScoreChart.drawChart($('#score-chart-container'));
+
+        // togglers
+        $('.toggler__handle').on('click', function(e) {
+            e.preventDefault();
+
+            $(this).closest(".toggler")
+                .find(".toggler__container")
+                .toggleClass("visible");
+
+            var text = $(this).text();
+            $(this).text($(this).data('toggler'));
+            $(this).data('toggler', text);
+        })
     });
 });
