606b0b5f72a7da36fd3a335151042ee56d609244
ghn <ghugon@gmail.com>
2015-01-12 22:33:48 +0100

    New profile page

--- user/models.py
+++ user/models.py
@@ -36,16 +36,20 @@ def user_post_init(sender, instance, **kwargs):
         if not hasattr(user, '_profile_cache'):
             from django.conf import settings
             if not getattr(settings, 'AUTH_PROFILE_MODULE', False):
-                raise Exception('You need to set AUTH_PROFILE_MODULE in your project settings')
+                raise Exception('You need to set AUTH_PROFILE_MODULE in your'
+                                'project settings')
             try:
                 app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
             except ValueError:
-                raise Exception('app_label and model_name should be separated by a dot in the AUTH_PROFILE_MODULE setting')
+                raise Exception('app_label and model_name should be separated '
+                                'by a dot in the AUTH_PROFILE_MODULE setting')
 
             try:
                 model = models.get_model(app_label, model_name)
                 if model is None:
-                    raise Exception('Unable to load the profile model, check AUTH_PROFILE_MODULE in your project settings')
+                    raise Exception('Unable to load the profile model, check '
+                                    'AUTH_PROFILE_MODULE in your project'
+                                    ' settings')
                 user._profile_cache, _ = model._default_manager.using(user._state.db).get_or_create(user=user)
                 user._profile_cache.user = user
             except (ImportError, ImproperlyConfigured):
--- user/views.py
+++ user/views.py
@@ -1,28 +1,59 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
-from django.forms.models import inlineformset_factory
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-import itertools
 
 from .forms import UserProfileForm, UserForm
 
 
 @login_required
 def index(request):
-    scores = list(itertools.chain.from_iterable([t.scores.all() for t in request.user.teams.all()]))
-    return render(request, 'user/profile.html', {
+    my_teams = request.user.teams.all()
+    competition_datas = []
+
+    for team in my_teams:
+        for competition in team.get_competitions():
+            head2head = team.get_head2head(competition)
+            last_results = team.get_recent_stats(competition, 10)
+            longest_streak = team.get_longest_streak(competition)
+            current_streak = team.get_current_streak(competition)
+
+            wins = team.get_wins(competition)
+            defeats = team.get_defeats(competition)
+            games = wins + defeats
+            score = team.get_score(competition)
+
+            competition_datas.append({
+                'team': team,
+                'head2head': head2head,
+                'last_results': last_results,
+                'longest_streak': longest_streak,
+                'current_streak': current_streak,
+                'games': games,
+                'wins': wins,
+                'defeats': defeats,
+                'score': score,
+                'competition': competition,
+                'stats_per_week': team.get_stats_per_week(),
+            })
+
+    context = {
         'user': request.user,
         'profile': request.user.get_profile(),
-        'scores': scores
-    })
+        'competition_datas': competition_datas[0],
+    }
+
+    return render(request, 'user/profile.html', context)
 
 
 @login_required
 def edit(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = UserProfileForm(request.POST, instance=request.user.get_profile())
+        profile_form = UserProfileForm(
+            request.POST,
+            instance=request.user.get_profile()
+        )
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -32,4 +63,9 @@ def edit(request):
         user_form = UserForm(instance=request.user)
         profile_form = UserProfileForm(instance=request.user.get_profile())
 
-    return render(request, 'user/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'user/edit.html', context)
