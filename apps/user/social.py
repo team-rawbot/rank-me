4583636a712fa679677a09bbfe5722a61851df91
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2016-04-19 19:16:48 +0200

    Make all avatars use HTTPS

--- apps/user/social.py
+++ apps/user/social.py
@@ -4,7 +4,10 @@ from .models import UserProfile
 def user_details(strategy, details, response, user=None, *args, **kwargs):
     # The Twitter image URL looks like ..._normal.jpg, to get the full-size
     # image we need to remove the _normal part
-    profile_image_url = response['profile_image_url'].replace('_normal.', '.')
+    profile_image_url = (
+        response['profile_image_url'].replace('_normal.', '.')
+                                     .replace('http:', 'https:')
+    )
 
     user_profile, _ = UserProfile.objects.get_or_create(user=user)
     user_profile.avatar = profile_image_url
